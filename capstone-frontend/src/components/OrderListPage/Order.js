2f5f89010b00a6c83f3ab4054e04a85f0199d818
charlotteg1 <charlotte.gould3@gmail.com>
2023-09-25 18:48:35 +0100

    fixing assign trucks

--- capstone-frontend/src/components/OrderListPage/Order.js
+++ capstone-frontend/src/components/OrderListPage/Order.js
@@ -5,13 +5,12 @@ import { OrderContext } from '../../containers/Container';
 const Order = ({order, updateOrderStatus, truckList}) => {
 
     const {setCurrentOrder}=useContext(OrderContext);
-    console.log("hello")
 
-    const handlePackOrder= async () =>{
+    const handlePackOrder= () =>{
         console.log("handling pack order button");
         setCurrentOrder(order);
         const truckId = assignTruck();
-        await updateOrderStatus(order.id, truckId, "ONGOING");
+        updateOrderStatus(order.id, truckId, "ONGOING")
     }
 
     console.log(truckList);
@@ -19,23 +18,31 @@ const Order = ({order, updateOrderStatus, truckList}) => {
     const calculateProductsSumSize = (orderToCalculate) => {
         let total = 0;
         orderToCalculate.products.map((product)=>{
-            ((product.size="SMALL")?total+=3 :(product.size="MEDIUM")?total+=7 : total+=10)
-            console.log(total);
+            ((product.size==="SMALL")?total+=3 :(product.size==="MEDIUM")?total+=7 : total+=10)
     })
     return total;
     
     }
 
     const assignTruck = () => {
+        //keep track of the totalSumSize, of the most filled truck.
+        let maxSumOfOrders = 0;
+        // keeps track of the most optimum truck found in map so far (highest total sum size)
+        let selectedTruckId = null;
+
         truckList.map((truck) => {
-            let sumOfOrdersOnTruck = truck.orders.map((orderToCalculate) => {
-                calculateProductsSumSize(orderToCalculate);
-            })
-            if ((sumOfOrdersOnTruck + calculateProductsSumSize(order)) < truck.maxCapacity) {
-                return truck.id;
-            }
-        })
-    }
+          let sumOfOrdersOnTruck = 0;
+          for (let i = 0; i < truck.orders.length; i++) {
+            sumOfOrdersOnTruck += calculateProductsSumSize(truck.orders[i]);}
+
+            // after sumOfOrdersOnTruck is calculated it checks if the order can fit in truck as well as checking if this "truck" is the most optimum truck available.
+          if(sumOfOrdersOnTruck + calculateProductsSumSize(order) <= truck.maxCapacity && sumOfOrdersOnTruck>maxSumOfOrders){
+            maxSumOfOrders= sumOfOrdersOnTruck;
+            selectedTruckId = truck.id
+          };}
+        )
+        return selectedTruckId;
+      };
 
     return(
     
@@ -43,9 +50,10 @@ const Order = ({order, updateOrderStatus, truckList}) => {
         <p>{order.id}</p>
         <p>{order.orderPriority ? "priority" : "non-priority"}</p>
         <p>{order.date}</p>
-        <Link to={`/OrderPage/${order.id}`}>
+        {order.status==="FINISHED"?( <Link to={`/OrderPage/${order.id}`}>
             <button onClick={(()=>handlePackOrder())}>Pack Order</button>
-        </Link>
+        </Link>): "order completed"}
+       
         </>
 
     );
