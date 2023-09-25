3a7294632310cb5d8f295e3701485f35cb8a489a
Sandra <sandramtzd@hotmail.com>
2023-09-25 15:15:06 +0100

    added logic to confirm packed button

--- capstone-frontend/src/components/OrderPage/Product.js
+++ capstone-frontend/src/components/OrderPage/Product.js
@@ -1,17 +1,53 @@
-const Product = ({product}) => {
-    return(
-        <>
-            <p>{product.name}</p>
-            {/* <p>{product.imageURL}</p> */}
-            <p>{product.category}</p>
-            <p>Location in warehouse: {product.productLocation}</p>
-            {product.heavy && (<p>heavy</p>)}
-            {product.fragile && (<p>fragile</p>)}
-            {product.flammable && (<p>flammable</p>)}
-            <button> Confirm item packed</button>
-            {/* report faulty button  */}
-        </>
-        )
-}
+import { useState } from "react";
+
+const Product = ({
+  product,
+  numberOfProductsPacked,
+  setNumberOfProductsPacked,
+}) => {
+  const [isPacked, setIsPacked] = useState(false);
+
+  //   const handleConfirmItem = () => {
+  //     setIsPacked(!isPacked);
+  //     if (isPacked) {
+  //       setNumberOfProductsPacked(numberOfProductsPacked + 1);
+  //       console.log(numberOfProductsPacked);
+  //     } else {
+  //       setNumberOfProductsPacked(numberOfProductsPacked - 1);
+  //       console.log(numberOfProductsPacked);
+  //     }
+  //   };
+
+  const handleItemPacked = () => {
+    setNumberOfProductsPacked(numberOfProductsPacked + 1);
+    setIsPacked(true);
+    console.log(numberOfProductsPacked);
+  };
+
+  const handleItemUnpacked = () => {
+    setIsPacked(false);
+    setNumberOfProductsPacked(numberOfProductsPacked - 1);
+    console.log(numberOfProductsPacked);
+  };
+
+  return (
+    <>
+      <p>{product.name}</p>
+      {/* <p>{product.imageURL}</p> */}
+      <p>{product.category}</p>
+      <p>Location in warehouse: {product.productLocation}</p>
+      {product.heavy && <p>heavy</p>}
+      {product.fragile && <p>fragile</p>}
+      {product.flammable && <p>flammable</p>}
+      {!isPacked ? (
+        <button onClick={handleItemPacked}> Confirm item packed</button>
+      ) : (
+        <button onClick={handleItemUnpacked}>Undo</button>
+      )}
+
+      {/* report faulty button  */}
+    </>
+  );
+};
 
 export default Product;
--- capstone-frontend/src/components/OrderPage/ProductList.js
+++ capstone-frontend/src/components/OrderPage/ProductList.js
@@ -1,25 +1,56 @@
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import Product from "./Product";
-import { OrderContext } from '../../containers/Container';
+import { OrderContext } from "../../containers/Container";
 
 const ProductList = () => {
-    const {currentOrder} = useContext(OrderContext);
+  const { currentOrder } = useContext(OrderContext);
+  const [numberOfProductsPacked, setNumberOfProductsPacked] = useState(0);
+  const [isOrderComplete, setIsOrderComplete] = useState(false);
 
-    if (!currentOrder || !currentOrder.products) {
-        return <p>Loading...</p>;
+  //   const allPacked = packedStatus.every(status => status === true);
+
+  if (!currentOrder || !currentOrder.products) {
+    return <p>Loading...</p>;
+  }
+
+  const mappedProducts = currentOrder.products.map((product, index) => {
+    return (
+      <div className="each-product" key={index}>
+        <Product
+          product={product}
+          numberOfProductsPacked={numberOfProductsPacked}
+          setNumberOfProductsPacked={setNumberOfProductsPacked}
+        />
+      </div>
+    );
+  });
+
+  useEffect(() => {
+    checkIsComplete();
+  }, [numberOfProductsPacked]);
+
+  const checkIsComplete = () => {
+    if (numberOfProductsPacked === currentOrder.products.length) {
+      setIsOrderComplete(true);
     }
+    if (
+      numberOfProductsPacked < currentOrder.products.length &&
+      isOrderComplete === true
+    ) {
+      setIsOrderComplete(false);
+    }
+  };
 
-    const mappedProducts = currentOrder.products.map((product, index)=>
-    {return(
-        <div className="each-product" key={index}>
-            <Product product={product} />
-        </div>
-    )})
-
-    return(
+  return (
     <>
-    {mappedProducts}
-    </>)
-}
+      {mappedProducts}
+      {isOrderComplete ? (
+        <button>Confirmed</button>
+      ) : (
+        "Please pack all the products"
+      )}
+    </>
+  );
+};
 
 export default ProductList;
